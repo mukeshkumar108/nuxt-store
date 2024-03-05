963e3af861f7da8fd46b9ccd1370b320e195d7bc
mukeshk <kaiser.kumar108@gmail.com>
2024-03-05 16:53:34 +0000

    wip: add pagination

---
+++ components/Pagination.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav aria-label="Pagination">
+        <button v-if="currentPage > 1" @click="goToPage(currentPage - 1)">Previous</button>
+        <button v-if="currentPage < totalPages" @click="goToPage(currentPage + 1)">Next</button>
+    </nav>
+</template>
+
+<script setup>
+const props = defineProps({
+    currentPage: Number,
+    totalPages: Number
+});
+
+const emit = defineEmits(['update:currentPage']);
+
+function goToPage(page) {
+    emit('update:currentPage', page);
+}
+</script>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -4,11 +4,13 @@
     <div v-for="product in products" :key="product.id" class="product">
       <h2>{{ product.attributes.product_name }}</h2>
       <h2>£{{ product.attributes.product_sell }}</h2>
-    </div>  
+    </div>
+    <Pagination :currentPage="currentPage" :totalPages="totalPages" @update:currentPage="updateCurrentPage" />  
   </div>
 </template>
 
 <script setup>
+import Pagination from '~/components/Pagination.vue';
 import { onMounted } from 'vue';
 import { useAsyncData } from '#app';
 import { useProductsStore } from '~/stores/products';
@@ -23,4 +25,13 @@ const { data: products } = useAsyncData('products', () => {
     }));
   });
 })
+
+const currentPage = ref(1);
+const pageSize = ref(12);
+const totalPages = computed(() => Math.ceil(products.value.length / pageSize.value));
+
+function updateCurrentPage(newPage) {
+  currentPage.value = newPage;
+}
+
 </script>
