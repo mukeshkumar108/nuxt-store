38be9f458f482e96fb5ebc5b21250cfccb1db0b8
mukeshk <kaiser.kumar108@gmail.com>
2024-03-11 10:54:50 +0000

    link productcard to single product page view

--- components/Productcard.vue
+++ components/Productcard.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="border border-neutral-200 rounded-md hover:shadow-lg max-w-sm p-4">
-    <div class="relative">
-      <img :src="product.attributes.image || 'https://place-hold.it/300x300/666/fff.png/000'" 
-           alt="Product image" 
-           class="block object-cover h-auto rounded-md aspect-square w-full" 
-           loading="lazy">
-    </div>
-    <div class="mt-4">
-      <h2 class="text-md font-bold">{{ product.attributes.product_name }}</h2>
-      <p class="text-gray-600">£{{ product.attributes.product_sell }}/m2</p>
-    </div>
+    <NuxtLink :to="`/products/${product.id}`">
+      <div class="relative">
+        <img :src="product.attributes.image || 'https://place-hold.it/300x300/666/fff.png/000'" 
+            alt="Product image" 
+            class="block object-cover h-auto rounded-md aspect-square w-full" 
+            loading="lazy">
+      </div>
+      <div class="mt-4">
+        <h2 class="text-md font-bold">{{ product.attributes.product_name }}</h2>
+        <p class="text-gray-600">£{{ product.attributes.product_sell }}/m2</p>
+      </div>
+    </NuxtLink>
   </div>
 </template>
 
